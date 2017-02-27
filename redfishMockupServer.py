ee37696cdf85ff1dda72bb3780c73497130e89e7
Joshua Giles <Joshua_Giles@dell.com>
2017-02-27 10:59:02 -0600

    Nasim's commit for changes based on Paul V discussion

--- redfishMockupServer.py
+++ redfishMockupServer.py
@@ -3,120 +3,120 @@
 # License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Mockup-Server/LICENSE.md
 
 # redfishMockupServer.py
-# v0.9.3
+# v0.9.5
 # tested and developed Python 3.4
+
 
 import urllib
 import cgi
 import sys
 import getopt
 import time
+import json
+import ssl
+import posixpath
 
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class RfMockupServer(BaseHTTPRequestHandler):
-        '''
-        returns index.json file for Serverthe specified URL
-        '''
-        server_version = "RedfishMockupHTTPD_v0.9.3"
+
+class MockupServer(BaseHTTPRequestHandler):
+    """
+    returns index.json file for Server in the specified URL
+    """
+    server_version = "RedfishMockupHTTPD_v0.9.5"
+
+    TOP_Header = "DFLT_RESPONSE_HEADERS.json"
+    SUB_Header = "SUB_HEADERS.json"
 
         def do_GET(self):
                 # for GETs always dump the request headers to the console
                 # there is no request data, so no need to dump that
                 print("   GET: Headers: {}".format(self.headers))
                 sys.stdout.flush()
-
-                rfile="index.json"
-                rfileXml="index.xml"
-                f=None
-
-                #xpath = self.translate_path(self.path)
-
-                if(self.path[0]=='/'):
-                        rpath=self.path[1:]
-                rpath = rpath.split('?',1)[0]
-                rpath = rpath.split('#',1)[0]
-                
-                # get the testEtagFlag and mockup directory path parameters passed in from the http server
-                apath=self.server.mockDir    # this is the real absolute path to the mockup directory
-                testEtagFlag=self.server.testEtagFlag
-                responseTime=self.server.responseTime
-                time.sleep(responseTime)
-
-                #print("-------apath; {}".format(apath))
-                #print("-------test: {}".format(testEtagFlag))
-
-                # form the path in the mockup of the file
-                #      old only support mockup in CWD:  apath=os.path.abspath(rpath)
-                fpath=os.path.join(apath,rpath, rfile)
-                fpathxml=os.path.join(apath,rpath, rfileXml)
-                #print("-------filepath:{}".format(fpath))
-                sys.stdout.flush()
-
-                if( os.path.isfile(fpath) is True):
+        self.command_delay(self.command)
+        if os.path.isfile(fpath) is True:
                         self.send_response(200)
-                        self.send_header("Content-type", "application/json")
+                self.send_header("Content-type", "application/json,charset=utf-8")
+                self.send_header("Additional_Headers", self.json_headers(self.TOP_Header, self.SUB_Header))
                         # special cases to test etag for testing
                         # if etag is returned then the patch to these resources should include this etag
-                        if( testEtagFlag is True ):
-                                if( self.path=="/redfish/v1/Systems/1" ):
+                if testEtagFlag is True:
+                        if self.path == "/redfish/v1/Systems/1":
                                         self.send_header("Etag", "W/\"12345\"")
-                                elif( self.path=="/redfish/v1/AccountService/Accounts/1" ):
-                                        self.send_header("Etag", "\"123456\"")
+                        elif self.path == "/redfish/v1/AccountService/Accounts/1":
+                                self.send_header("Etag", "W/\"123456\"")
                         self.end_headers()
-                        f=open(fpath,"r")
+                f = open(fpath, "r")
                         self.wfile.write(f.read().encode())
                         f.close()
-                elif( os.path.isfile(fpathxml) is True):
+        elif os.path.isfile(fpathxml) is True:
                         self.send_response(200)
-                        self.send_header("Content-type", "application/xml")
+                self.send_header("Content-type", "application/xml,charset=utf-8")
+                self.send_header("Additional_Headers", self.json_headers(self.TOP_Header, self.SUB_Header))
                         self.end_headers()
-                        f=open(fpathxml,"r")
+                f = open(fpathxml, "r")
                         self.wfile.write(f.read().encode())
                         f.close()
                 else:
                         self.send_response(404)
                         self.end_headers()
                         
+    def do_HEAD(self):
+            print("   HEAD: Headers: {}".format(self.headers))
+            if "content-length" in self.headers:
+                    len1 = int(self.headers["content-length"])
+                    dataa = self.rfile.read(len1)
+                    print("   HEAD: Data: {}".format(dataa))
+
+            self.command_delay(self.command)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json,charset=utf-8")
+            self.send_header("Additional_Headers", self.json_headers(self.TOP_Header, self.SUB_Header))
+            self.end_headers()
+
         def do_PATCH(self):
                 print("   PATCH: Headers: {}".format(self.headers))
-                if( "content-length" in self.headers ):
-                        len=int(self.headers["content-length"])
-                        dataa=self.rfile.read(len)
+            if "content-length" in self.headers:
+                    len1 = int(self.headers["content-length"])
+                    dataa = self.rfile.read(len1)
                         print("   PATCH: Data: {}".format(dataa))
-                responseTime=self.server.responseTime
-                time.sleep(responseTime)
+            self.command_delay(self.command)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json,charset=utf-8")
+            self.send_header("AAdditional_Headers", self.json_headers(self.TOP_Header, self.SUB_Header))
 
-                self.send_response(204)
                 self.end_headers()
                 
         def do_POST(self):
                 print("   POST: Headers: {}".format(self.headers))
-                if( "content-length" in self.headers ):
-                        len=int(self.headers["content-length"])
-                        dataa=self.rfile.read(len)
+            if "content-length" in self.headers:
+                    len1 = int(self.headers["content-length"])
+                    dataa = self.rfile.read(len1)
                         print("   POST: Data: {}".format(dataa))
-                responseTime=self.server.responseTime
-                time.sleep(responseTime)
 
-                self.send_response(204)
+            self.command_delay(self.command)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json,charset=utf-8")
+            self.send_header("Additional_Headers", self.json_headers(self.TOP_Header, self.SUB_Header))
+
                 self.end_headers()
                 
         def do_DELETE(self):
                 print("DELETE: Headers: {}".format(self.headers))
-                if( "content-length" in self.headers ):
+            if "content-length" in self.headers:
                         # Deletes don't have data, so this doesnt execute
-                        len=int(self.headers["content-length"])
-                        dataa=self.rfile.read(len)
+                    len1 = int(self.headers["content-length"])
+                    dataa = self.rfile.read(len1)
                         print("DELETE: Data: {}".format(dataa))
-                responseTime=self.server.responseTime
-                time.sleep(responseTime)
+            self.command_delay(self.command)
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json,charset=utf-8")
+            self.send_header("Additional_Headers", self.json_headers(self.TOP_Header, self.SUB_Header))
                 
-                self.send_response(204)
                 self.end_headers()
                 
-        # this is currently not used
         def translate_path(self, path):
                 """
                 Translate a /-separated PATH to the local filename syntax.
@@ -125,8 +125,8 @@ class RfMockupServer(BaseHTTPRequestHandler):
                 probably be diagnosed.)
                 """
                 # abandon query parameters
-                path = path.split('?',1)[0]
-                path = path.split('#',1)[0]
+                path = path.split('?', 1)[0]
+                path = path.split('#', 1)[0]
                 path = posixpath.normpath(urllib.unquote(path))
                 words = path.split('/')
                 words = filter(None, words)
@@ -134,39 +134,106 @@ class RfMockupServer(BaseHTTPRequestHandler):
                 for word in words:
                         drive, word = os.path.splitdrive(word)
                         head, word = os.path.split(word)
-                        if word in (os.curdir, os.pardir): continue
+                        if word in (os.curdir, os.pardir):
+                            continue
                 path = os.path.join(path, word)
                 return path
+
+    def file_path(self):
+        global fpath, tpath, rpath, apath, xpathxml, rfileXml, fpathxml
+        rfile = "index.json"
+        rfileXml = "index.xml"
+        Tfile = "TIME.json"
+        if self.path[0] == '/':
+            rpath = self.path[1:]
+            rpath = rpath.split('?', 1)[0]
+            rpath = rpath.split('#', 1)[0]
+            apath = self.server.mockDir  # this is the real absolute path to the mockup directory
+            # form the path in the mockup of the file
+            #      old only support mockup in CWD:  apath=os.path.abspath(rpath)
+            fpath = os.path.join(apath, rpath, rfile)
+            fpathxml = os.path.join(apath, rpath, rfileXml)
+            tpath = os.path.join(apath, rpath, Tfile)
+
+    def command_delay(self, command):
+        self.file_path()
+        delay_time = responseTime
+        if not tflag:
+            if dflttimeflag:
+                if os.path.isfile(tpath):
+                    print("There is a TIME.json in the folder", rpath)
+                    sys.stdout.flush()
+                    with open(tpath, "r+") as jfile:
+                        try:
+                            djson = json.load(jfile)
+                            delay_time = float(djson.get(self.command + "time", dflttime))
+                        except ValueError:
+                            delay_time = dflttime
+                else:
+                    delay_time = dflttime
+        time.sleep(delay_time)
+
+    def json_headers(self, top_header_name, sub_header_name):
+        TOP_Header_Path = os.path.join(self.server.mockDir, top_header_name)
+        SUB_Header_Path = ""
+        merged_header = {}
+        if self.path[0] == '/':
+            rpath = self.path[1:]
+            rpath = rpath.split('?', 1)[0]
+            rpath = rpath.split('#', 1)[0]
+            SUB_Header_Path = os.path.join(self.server.mockDir, rpath, sub_header_name)
+        TOP_Header = ""
+        SUB_Header = ""
+        if os.path.isfile(TOP_Header_Path):  # merging headers
+            TOP_Header = json.load(open(TOP_Header_Path))
+        if os.path.isfile(SUB_Header_Path):
+            SUB_Header = json.load(open(SUB_Header_Path))
+        if TOP_Header:
+            merged_header = {key: value for key, value in TOP_Header.items()}
+        if SUB_Header:
+            for (key, value) in SUB_Header.items():
+                merged_header[key] = value
+        if merged_header:
+            return merged_header
+        return 'No Additional Header'
 
 def usage(program):
         print("usage: {}   [-h][-P][-H <hostIpAddr>:<port>]".format(program))
         print("      -h --help      # prints usage ")
         print("      -L --Load      # <not implemented yet>: load and Dump json read from mockup in pretty format with indent=4")
         print("      -H <IpAddr>   --Host=<IpAddr>    # hostIP, default: 127.0.0.1")
+        print("      -d <responseTime> --dflttime=<executionTime> # time added to respond to any API from Time.json")   # Need to fix
         print("      -P <port>     --Port=<port>      # port:  default is 8000")
         print("      -D <dir>,     --Dir=<dir>        # the to the mockup directory. It may be relative to CWD")
         print("      -T --TestEtag  # etag testing--enable returning etag for certain APIs for testing.  See Readme")
         print("      -t <responseTime> --time=<executionTime> # time added to respond to any API")
+        print("-" * 100)
+        print("       Exit : ^C <Ctrl+C> ")
         sys.stdout.flush()
 
 
 def main(argv):
-        hostname="127.0.0.1"
-        port=8000
-        load=False
-        program=argv[0]
+        global  testEtagFlag
+        global responseTime, tflag, dflttime, dflttimeflag, timePath, hostname, mockDir
+        hostname = "127.0.0.1"
+        port = 8000
+        load = False
+        program = argv[0]
         print(program)
-        mockDirPath=None
-        mockDir=None
-        testEtagFlag=False
-        responseTime=0
+        mockDirPath = os.getcwd()
+        mockDir = os.path.realpath(mockDirPath)
+        testEtagFlag = False
+        responseTime = 0
+        tflag = False
+        dflttime = 0
+        dflttimeflag = False
+        tTime = 0
+        DirFlag = 0
         
         try:
-                opts, args = getopt.getopt(argv[1:],"hLTH:P:D:t:",["help","Load", "TestEtag", "Host=", "Port=", "Dir=",
-                                                                   "time="])
+                opts, args = getopt.getopt(argv[1:], "hLTH:P:D:t:d:", ["help", "Load", "TestEtag", "Host=", "Port=", "Dir=", "time=", "dflttime="])
         except getopt.GetoptError:
-                #usage()
-                print("Error parsing options", file=sys.stderr)
+                print("Error parsing options")
                 sys.stderr.flush()
                 usage(program)
                 sys.exit(2)
@@ -176,59 +243,80 @@ def main(argv):
                         usage(program)
                         sys.exit(0)
                 elif opt in ("L", "--Load"):
-                        load=True
+                        load = True
                 elif opt in ("-H", "--Host"):
-                        hostname=arg
+                        hostname = arg
                 elif opt in ("-P", "--Port"):
-                        port=int(arg)
+                        port = int(arg)
                 elif opt in ("-D", "--Dir"):
-                        mockDirPath=arg
+                        DirFlag = 1
+                        mockDirPath = arg
+                        mockDir = os.path.realpath(mockDirPath)  # creates real full path including path for the arg
                 elif opt in ("-T", "--TestEtag"):
-                        testEtagFlag=True
+                        testEtagFlag = True
                 elif opt in ("-t", "--time"):
-                        responseTime=float(arg)
+                        tflag = True
+                        tTime = float(arg)
+                elif opt in ("-d", "--dflttime"):
+                        dflttimeflag = True
+                        dflttime = float(arg)
                 else:
-                        print('unhandled option', file=sys.stderr)
+                        print('unhandled option',)
                         sys.exit(2)
 
-        print ('program: ', program)
-        print ('Hostname:', hostname)
-        print ('Port:', port)
-        print ("dir path specified by user:{}".format(mockDirPath))
-        print ("response time: {} seconds".format(responseTime))
-        sys.stdout.flush()
-
-        # check if mockup path was specified.  If not, use current working directory
-        if mockDirPath is None:
-                mockDirPath=os.getcwd()
-
-        #create the full path to the top directory holding the Mockup  
-        mockDir=os.path.realpath(mockDirPath) #creates real full path including path for CWD to the -D<mockDir> dir path
-        
-        print ("Serving Mockup in abs real directory path:{}".format(mockDir))
-
-        # check that we have a valid tall mockup--with /redfish in mockDir before proceeding
-        slashRedfishDir=os.path.join(mockDir, "redfish")
-        if os.path.isdir(slashRedfishDir) is not True:
-                print("ERROR: Invalid Mockup Directory--no /redfish directory at top. Aborting", file=sys.stderr)
+        # check that that -d and -t were not both specified
+        if (tflag is True) and (dflttimeflag is True):
+                print("ERROR: -t and -d dflttime not valid together here")
                 sys.stderr.flush()
                 sys.exit(1)
 
-        myServer=HTTPServer((hostname, port), RfMockupServer)
-
-        # save the test flag, and real path to the mockup dir for the handler to use
-        myServer.mockDir=mockDir
-        myServer.testEtagFlag=testEtagFlag
-        myServer.responseTime=responseTime
-        #myServer.me="HELLO"
-        
-        print( "Serving Redfish mockup on port: {}".format(port))
+        print('program: ', program)
         sys.stdout.flush()
+        print('Hostname:', hostname)
+        sys.stdout.flush()
+        print('Port:', port)
+        sys.stdout.flush()
+
+        if DirFlag:
+                print("dir path specified by user:{}".format(mockDirPath))
+        else:
+                print("dir path specified by user: None")
+        sys.stdout.flush()
+        
+        if tflag:
+            responseTime = tTime
+
+        print("Serving Mockup in abs real directory path:{}".format(mockDir))
+        sys.stdout.flush()
+        # check that we have a valid tall mockup--with /redfish in mockDir before proceeding
+        slashRedFishDir = os.path.join(mockDir, "redfish")
+        if os.path.isdir(slashRedFishDir) is not True:
+                print("ERROR: Invalid Mockup Directory--no /redfish directory at top. Aborting")
+                sys.stderr.flush()
+                sys.exit(1)
+
+        myServer = HTTPServer((hostname, port), MockupServer)
+        # save the test flag, and real path to the mockup dir for the handler to use
+        # myServer.socket = ssl.wrap_socket(myServer.socket, "cert.pem", "key.pem", True) #todo add certificate
+
+        myServer.mockDir = mockDir
+        myServer.testEtagFlag = testEtagFlag
+        myServer.responseTime = responseTime
+        # myServer.me="HELLO"
+        
+        print("Serving Redfish mockup on port: {}".format(port))
+        sys.stdout.flush()
+
+        if dflttimeflag:
+            print("System Respose default Time is {} seconds".format(dflttime))
+        else:
+            print("System response time: {} seconds".format(responseTime))
+        sys.stdout.flush()
+
         try:
                 myServer.serve_forever()
-        except KeyboardInterupt:
+        except KeyboardInterrupt:
                 pass
-
         myServer.server_close()
         print("Shutting down http server")
         sys.stdout.flush()
@@ -237,12 +325,3 @@ def main(argv):
 # the below is only executed if the program is run as a script
 if __name__ == "__main__":
         main(sys.argv)
-
-'''
-TODO:
-1. add -L option to load json and dump output from python dictionary
-2. add authentication support -- note that in redfish some api don't require auth
-3. add https support
-
-
-'''
